227e1669889019f881a082b06a7474f74658cbdf
unknown <amishkumar562@gmail.com>
2023-10-18 17:30:24 +0530

    added action

--- tracker.py
+++ tracker.py
@@ -130,5 +130,14 @@ class MuliCameraTracker:
 
 if __name__ == "__main__":
     object_tracker = Tracker('sort')
-    # pass
-    print(object_tracker([[0.1, 0.2, 0.3, 0.4, 0.5, 0], [0.2, 0.3, 0.4, 0.5, 0.6, 1]]))
+    print("test case started")
+    print("test case 1")
+    test = object_tracker([[0.1, 0.2, 0.3, 0.4, 0.5, 0], [0.2, 0.3, 0.4, 0.5, 0.6, 1]])
+    for *xyxy, conf, cls, ID in test:
+        print("xyxy: ", xyxy)
+        print("conf: ", conf)
+        print("cls: ", cls)
+        print("ID: ", ID)
+        print("\n")
+    print("test case ended")
+    
